e38c1a879f4ad0547a4a62f1f16739ced3c0c810
m03r <m03r@m03r.net>
2024-03-17 23:27:17 +0300

    Fix README & benchmarks

--- tests/Benchmark/PerformanceBench.php
+++ tests/Benchmark/PerformanceBench.php
@@ -30,19 +30,35 @@ class PerformanceBench
         };;
     }
 
-    public function benchNoParams() {
+    public function benchNoParams()
+    {
         $this->db->buildQuery('SELECT * FROM table WHERE id = 1 AND name = \'test\'', []);
     }
 
-    public function benchWithParams() {
+    public function benchWithParams()
+    {
         $this->db->buildQuery('SELECT * FROM table WHERE id = ?d AND name = ?', [1, 'test']);
     }
 
-    public function benchWithCondition() {
+    public function benchWithCondition()
+    {
         $this->db->buildQuery('SELECT * FROM table WHERE id = ?d AND name = ?{ AND block = ?d}', [1, 'test', 1]);
     }
 
-    public function benchWithSkipCondition() {
+    public function benchWithSkipCondition()
+    {
         $this->db->buildQuery('SELECT * FROM table WHERE id = ?d AND name = ?{ AND block = ?d}', [1, 'test', $this->db->skip()]);
+    }
+
+    public function benchWithManyParamsAndBlocks()
+    {
+        $this->db->buildQuery('SELECT * FROM table WHERE id = ?d AND name = ?{ AND block = ?d} AND age = ?d 
+AND age2 = ?d AND age3 = ?d{ AND age4 = ?d }AND age5 = ?d AND age6 = ?d 
+AND age7 = ?d{ AND age8 = ?d AND age9 = ?d }AND age10 = ?d AND test_value IN (?a)',
+            [1, 'test',
+                1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, ['test', 'THIS IS REALLY LONG STRING',
+                'LONG LONG long long LONG long LONG LONG long long LONG long LONG LONG long long LONG long 
+                LONG LONG long long LONG long LONG LONG long long LONG long LONG LONG LONG long long LONG long 
+                LONG LONG long long LONG long LONG LONG long long LONG long LONG LONG long long LONG long ']]);
     }
 }
